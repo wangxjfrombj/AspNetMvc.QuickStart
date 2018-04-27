108e7f65c4e925618a6d1e811b4bd29b95ea978c
wangxj-pc\wangxj <52383457@163.com>
2018-04-27 12:22:16 +0800

    commit code

---
+++ AspNetMvc.QuickStart/Models/StudentDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AspNetMvc.QuickStart.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
